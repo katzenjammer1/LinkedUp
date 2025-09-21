9a2435f9f127351deea8bb0835678bf6a1205884
pirrr-h <156934647+pirrr-h@users.noreply.github.com>
2025-09-21 14:11:06 -0700

    Update matchingService.js
    added extra calculation and changed weights

--- src/lib/matchingService.js
+++ src/lib/matchingService.js
@@ -53,7 +53,7 @@ const calculateCompatibility = (user1, user2) => {
   const commonInterests = interests1.filter((interest) =>
     interests2.includes(interest)
   ).length;
-  score += commonInterests * 10;
+  score += commonInterests * 50;
 
   // Activity overlap
   const activities1 = user1.preferredActivities || [];
@@ -61,13 +61,22 @@ const calculateCompatibility = (user1, user2) => {
   const commonActivities = activities1.filter((activity) =>
     activities2.includes(activity)
   ).length;
-  score += commonActivities * 15;
+  score += commonActivities * 40;
 
   // Available days overlap
   const days1 = user1.availableDays || [];
   const days2 = user2.availableDays || [];
   const commonDays = days1.filter((day) => days2.includes(day)).length;
-  score += commonDays * 5;
+  score += commonDays * 10;
+
+  let difference = Math.abs(user1.age - user2.age);
+  if (difference !== 0){
+    score += Math.max(1, 30/difference); 
+  }
+  else{
+    score += 30;
+  }
+
 
   return score;
 };
